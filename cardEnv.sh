af466022b98479c459dacc268446a3bc42386f63
Kevin Wierman <kwierman@emailunc.edu>
2014-01-07 12:58:33 -0500

    Still more structural changes

---
+++ cardEnv.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+#!cardEnv.sh
+
+export PYTHONPATH=@CMAKE_INSTALL_PREFIX@\:$PYTHONPATH
--- cmake/python.cmake
+++ cmake/python.cmake
@@ -1,10 +1,25 @@
-macro(add_python_target tgt)
+#macros for 
+#
+#macro(add_python_target tgt)
+#  foreach(file ${ARGN})
+#    set(OUT ${CMAKE_CURRENT_BINARY_DIR}/Source/${file}.pyc)
+#    list(APPEND OUT_FILES ${OUT})
+#    add_custom_command(OUTPUT ${OUT}
+#        COMMAND python -mcompileall ${file}.py)
+#  endforeach()
+#  add_custom_target(${tgt} ALL
+#    DEPENDS ${OUT_FILES} )
+#endmacro()
+
+
+macro(python_add_packages)
   foreach(file ${ARGN})
-    set(OUT ${CMAKE_CURRENT_BINARY_DIR}/${file}.pyo)
-    list(APPEND OUT_FILES ${OUT})
-    add_custom_command(OUTPUT ${OUT}
-        COMMAND python -mcompileall)
+  	message(${file})
   endforeach()
-  add_custom_target(${tgt} ALL
-    DEPENDS ${OUT_FILES}
+endmacro()
+
+macro(python_add_scripts)
+  foreach(file ${ARGN})
+  	message(${file})
+  endforeach()
 endmacro()
